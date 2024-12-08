14cd6885bf2825bdac8c4fc0ed56d9b003a8be2a
MandemGibson <themaingib@gmail.com>
2024-12-08 02:59:57 +0000

    redirecting endpoint

--- server/app.ts
+++ server/app.ts
@@ -1,15 +1,26 @@
 import express from "express";
 import mongoose from "mongoose";
 import { config } from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
 
 import { urlRouter } from "./routes/url.route";
+import { redirectToOriginalHandler } from "./controllers/url.controller";
 config();
 
 const uri = process.env.MONGO_URI as string;
-
 const app = express();
+
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 app.use("/api/urls/shorten", urlRouter);
+app.use("/:shortCode", redirectToOriginalHandler);
 
 async function connectDB() {
   try {
@@ -23,7 +34,7 @@ async function connectDB() {
   } catch (error) {
     console.error("Error connecting to mongoDB: ", error);
     process.exit(1);
-  } 
+  }
 }
 
 connectDB();
--- server/controllers/url.controller.ts
+++ server/controllers/url.controller.ts
@@ -1,3 +1,4 @@
+import { Url } from "../models/Url";
 import {
   createShortUrl,
   deleteShortUrl,
@@ -16,7 +17,9 @@ export const createShortUrlHandler = async (
 ): Promise<any> => {
   try {
     const { url } = req.body;
-    if (!url) return res.status(400).json({ message: "Please provide url" });
+    if (!url || url === "")
+      return res.status(400).json({ message: "Please provide url" });
+
     const newUrl = await createShortUrl(url);
 
     res.status(201).json(newUrl);
@@ -138,5 +141,25 @@ export const deleteShortUrlHandler = async (
     res.status(200).json({ message: "Deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: "Error deleting url: ", error });
+  }
+};
+
+export const redirectToOriginalHandler = async (
+  req: Request,
+  res: Response,
+  _next: NextFunction
+): Promise<any> => {
+  const { shortCode } = req.params;
+
+  try {
+    const urlEntry = await getShortUrl(shortCode);
+    if (!urlEntry) {
+      return res.status(404).send("Short URL not found.");
+    }
+
+    res.redirect(urlEntry.url);
+  } catch (error) {
+    console.error("Error during redirection:", error);
+    res.status(500).send("Server error.");
   }
 };
